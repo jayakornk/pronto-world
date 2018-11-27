6cf21eee796d853f72c9c261e8b79d06e8230003
Bacon <bacon@prontomarketing.com>
2018-11-27 18:22:39 +0700

    removed unused code

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const sourcemaps = require('gulp-sourcemaps');
 const LessAutoprefix = require('less-plugin-autoprefix');
-const LessPluginCleanCSS = require('less-plugin-clean-css')
 
 const autoprefix = new LessAutoprefix({ browsers: ['last 5 versions', 'ie 10'] })
-// const cleancss = new LessPluginCleanCSS({ advanced: true });
 const cleanCSS = require('gulp-clean-css');
 
 // Where your Less files are located
@@ -23,16 +21,6 @@ gulp.task('less', function() {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(dstDir));
 });
-
-// gulp.task('less', function() {
-//   gulp.src(`${srcDir}/*.less`)
-//     .pipe(sourcemaps.init())
-//     .pipe(less({
-//       plugins: [autoprefix, cleancss],
-//     }))
-//     .pipe(sourcemaps.write())
-//     .pipe(gulp.dest(dstDir));
-// });
 
 gulp.task('default', ['less'], function() {
   gulp.watch(`${srcDir}/*.less`, ['less']);
